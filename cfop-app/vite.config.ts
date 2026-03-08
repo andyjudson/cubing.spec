16614a210864b415ea9ea8951d8031308b408167
Andy Judson <andy.judson@gmail.com>
2026-03-08 01:38:23 +0000

    fix: remove optimizeDeps exclude for cubing/scramble to fix production worker

--- cfop-app/vite.config.ts
+++ cfop-app/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/cubing.spec/',
-  optimizeDeps: {
-    exclude: ['cubing/scramble'],
-  },
   worker: {
     format: 'es',
   },
